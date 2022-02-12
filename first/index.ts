0140b74c22bebb04384e7e65ec8c4d1b45508980
Øystein Lorentzen Vesth <oystein.vesth@gmail.com>
2022-02-12 10:36:43 +0100

    testing

---
+++ first/index.ts
@@ -0,0 +1,19 @@
+import * as core from '@actions/core'
+import { context, getOctokit } from '@actions/github'
+
+const owner = context.payload.repository.owner
+const repo = context.payload.repository.name
+const run_id = context.runId
+
+const run = async () => {
+    const octokit = getOctokit(process.env.GITHUB_TOKEN)
+
+    console.log(core.getInput('title'))
+    console.log('THE CONTEXT')
+    console.log(JSON.stringify(context, undefined, 2))
+    console.log('')
+    console.log('=====================')
+    console.log('')
+}
+
+run()
